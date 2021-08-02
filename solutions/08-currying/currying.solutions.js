f76b24dfe8c94388713e1db45bd397d196b5e010
Matina <matinaspatsos@gmail.com>
2021-08-02 18:49:43 -0400

    Clsoures and currying solutions

---
+++ solutions/08-currying/currying.solutions.js
@@ -0,0 +1,25 @@
+/**
+ * Write a function called "multiplier" which accepts two parameters.
+ * If the function is passed both parameters, it should return the product of the two.
+ * If the function is only passed one parameter, it should return a function which can later be passed another
+ * parameter to return the product.
+ * You will have to use closures and arguments to solve this.
+ *
+ * @example
+ * multiplier(3,4); // 12
+ * multiplier(3)(4); // 12
+ * multiplier(3); // function(){}....
+ *
+ * @param {number} a
+ * @param {number} b
+ */
+const multiplier = (a, b) => {
+  if (a && b) {
+    // or if (a !== undefined && b !== undefined) {
+    return a * b;
+  } else {
+    return (b) => a * b;
+  }
+};
+
+export { multiplier };
